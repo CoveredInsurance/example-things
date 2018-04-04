60c590b2d8a11424277a1609fb62a4e363b87864
Aaron O. Ellis <aaronoellis@gmail.com>
2018-04-04 15:34:12 -0600

    Added server boilerplate

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const grpc = require('grpc');
+
+const { ThingsService } = require('./js/things_grpc_pb.js');
+
+
+let get = (call, done) => {
+  return done({
+    code: grpc.status.UNIMPLEMENTED,
+    message: 'Unimplemented'
+  });
+};
+
+// createServer creates a new grpc Server with EDI endpoint handlers
+let server = new grpc.Server();
+server.addService(ThingsService, {
+  get: get
+});
+
+server.bind('0.0.0.0:0', grpc.ServerCredentials.createInsecure());
+server.start();
