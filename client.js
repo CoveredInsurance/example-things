62d7d780c5f5eef56bdc83d7259c88eeb677adba
Robby Hajek <hajek.rob@gmail.com>
2018-04-05 12:55:34 -0600

    revert

--- client.js
+++ client.js
@@ -7,11 +7,6 @@ const { Thing } = require('./js/things_pb.js');
 
 let client = new ThingsClient('0.0.0.0:23478', grpc.credentials.createInsecure());
 
-let thingRq = new Thing();
-thingRq.setId(2)
-
 client.get(thingRq, (err, res) => {
   console.log(err, res)
-  let id = res.getId();
-  console.log(id)
 });
--- index.js
+++ index.js
@@ -5,34 +5,7 @@ const grpc = require('grpc');
 const { ThingsService } = require('./js/things_grpc_pb.js');
 const { Thing } = require('./js/things_pb.js');
 
-let things = [
-  {
-    id: 1,
-    name: 'Test Name',
-    desc: 'Test Desc...'
-  },
-  {
-    id: 2,
-    name: 'Test Name2',
-    desc: 'Test Desc2...'
-  },
-]
-
-let get = (call, done) => {
-  let thingRs = call.request;
-  let id = thingRs.getId()
-  let thingMatch;
-  
-  for (let thing of things) {
-    if (thing.id === id) {
-      thingMatch = thing;
-    }
-  }
-  thingRs.setId(thingMatch.id);
-  thingRs.setName(thingMatch.name);
-  thingRs.setDesc(thingMatch.desc);
-  return done(null, thingRs)
-};
+let get = (call, done) => {};
 
 // createServer creates a new grpc Server with EDI endpoint handlers
 let server = new grpc.Server();
@@ -41,5 +14,5 @@ server.addService(ThingsService, {
 });
 
 server.bind('0.0.0.0:23478', grpc.ServerCredentials.createInsecure());
-console.log('Server started!!!!');
+console.log('Server started!');
 server.start();
