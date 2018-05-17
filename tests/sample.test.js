47d4c47c03c2cbcf6e0bd1fa93417a78222fd285
Robby Hajek <hajek.rob@gmail.com>
2018-05-17 11:09:51 -0600

    added tests dir

---
+++ tests/sample.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('Sample test describe block', function() {
+  before(() => {
+    console.log('Sample before block executing...')
+  });
+
+  it('should test', done => {
+    expect(true).to.be.true;
+  })
+})
